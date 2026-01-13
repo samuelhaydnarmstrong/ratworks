e406b79d9f9145c2173d58f88d588e31297f37b5
Samuel Armstrong <samuelhaydnarmstrong@outlook.com>
2026-01-13 18:33:22 +0000

    Testing deployment still works

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,5 @@ import { Construction } from "@mui/icons-material";
 import { AppBar, Box, Paper, Toolbar, Typography } from "@mui/material";
 
 export default function Home() {
-  return (
-    <Box>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Ratworks
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box sx={{ width: "fit-content", margin: "20vh auto", display: "flex", alignItems: "center", flexDirection: "column" }}>
-        <Construction sx={{ fontSize: "8em", color: "#646464" }} />
-        <Typography sx={{ fontSize: "1.5em" }}>Under Construction</Typography>
-      </Box>
-    </Box>
-  );
+  return <p>Under Construction</p>;
 }
