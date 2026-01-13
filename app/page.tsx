46a9d4aed003bcee5748e9f8aa991bbeb72708dc
Samuel Armstrong <samuelhaydnarmstrong@outlook.com>
2026-01-13 23:00:59 +0000

    Complete change to Godot test

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Construction } from "@mui/icons-material";
-import { AppBar, Box, Paper, Toolbar, Typography } from "@mui/material";
-
-export default function Home() {
-  return <p>Under Construction</p>;
-}
