888e243999e0284a96f483e54a095402c8f29f45
Samuel Armstrong <samuelhaydnarmstrong@outlook.com>
2026-01-16 21:21:30 +0000

    Rail placement, shadow rail, coast connecting

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Ratworks
 
+## Getting Started
+
+1. Install Godot from https://godotengine.org/download/windows/. This is the Game Engine we're using and is required to make most changes to the game.
+2. Install Git from https://git-scm.com/. This is a version control system, similar to what you'd find in apps like Word, but it works on folders of files.
+3. Open your Command Prompt tool of choice and run the command `git clone https://github.com/samuelhaydnarmstrong/ratworks.git`. This command fetches all the files from the Cloud and copies them to your computer.
+4. Run Godot and select **Import**, select the folder that was downloaded from Git, and follow the steps to import the project.
+
+![alt text](image.png)
+
 ## Running
 
-To run the build locally it must be exported from Godot using the HTML exporter. If you're doing a new build then clear down the folder first.
+To run the project locally follow step 1 in the deployment steps and then use the command `npm run serve`. This uses a package called Serve to run a local file server which hosts the `build` folder.
 
 ## Deploying
 
-1. Clear down everything already in the `build` folder
-2. Export the project from Godot using the Web preset (without Debug) into the `build` folder
-3. Rename the HTML file to `index.html`, it's probably exported as `ratworks.html`. Would be good to fix this somehow.
-4. Push to Github. Vercel should automatically pick up the merge and deploy it. Note that it doesn't actually use `serve` it just statically serves the files itself.
+1. Export the project from Godot using the Web preset (without Debug) into the `build` folder. The export name should be `index` as this means it'll override what's already there, and it'll name the main file `index.html` which means it'll be the default landing page for `ratworks.co.uk` when it's deployed. If we gave it a name then we'd need to have a redirect.
+2. Commit the changes to `main`.
+3. Push to Github. Vercel should automatically pick up the merge and deploy it. Note that it doesn't actually use `serve` it just statically serves the files itself.
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,48 +1,54 @@
 extends Node
 
-var flagToSet = 0
-var start
-var end
+var shadowRail = PackedVector2Array([Vector2(0,0), Vector2(0,0)])
+var isRailValid
+
+var MAX_RAIL_LENGTH = 100
+
+# get_viewport().get_visible_rect().size.x
+#var x = floor(event.position.x / 64)
+#var y = floor(event.position.y / 64)
+#$TileMapLayer.set_cell(Vector2i(x, y), 1, Vector2i(2,2),1)	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _input(event):
-	if event is InputEventMouseButton and event.is_pressed() == true:
-		print("Click event")
-		# get_viewport().get_visible_rect().size.x
-		var x = floor(event.position.x / 64)
-		var y = floor(event.position.y / 64)
+	if($ShadowRail.visible):
+		shadowRail.set(1,event.position)
+		$ShadowRail.points = shadowRail
+		if (shadowRail.get(0).distance_to(event.position) > MAX_RAIL_LENGTH):
+			$ShadowRail.set_default_color(Color(1, 0, 0, 1))
+			isRailValid = false
+		else:
+			$ShadowRail.set_default_color(Color(0, 1, 0, 1))
+			isRailValid = true
 		
-		if (flagToSet == 1):
-			print("Setting start")
-			start = event.position
-			$TileMapLayer.set_cell(Vector2i(x, y), 1, Vector2i(2,2),1)
-			flagToSet = -1;
-		elif (flagToSet == 2):
-			print("Setting end")
-			end = event.position
-			$TileMapLayer.set_cell(Vector2i(x, y), 1, Vector2i(2,2),1)
-			flagToSet = -1;
-
-func _on_flag_1_button_pressed() -> void:
-	flagToSet = 1
-
-
-func _on_flag_2_button_pressed() -> void:
-	flagToSet = 2
-
-
-func _on_draw_line_button_pressed() -> void:
-	var line := Line2D.new()
-	line.width = 3
-	line.default_color = Color.CYAN
-	line.add_point(start)
-	line.add_point(end)
-	line.z_index = 10
-	add_child(line)
+	if event is InputEventMouseButton and event.is_pressed() == true:
+		if event.get_button_index() == 1:
+			var x = floor(event.position.x / 64)
+			var y = floor(event.position.y / 64)
+			
+			if ($Rail.get_point_count() and isRailValid and $ShadowRail.visible): # If we're placing rail
+				
+				$Rail.add_point(Vector2(event.position.x, event.position.y))
+				shadowRail.set(0, event.position)
+				shadowRail.set(0, event.position)
+				
+				# Check if the player has clicked on a EAST coast tile
+				if($TileMapLayer.get_cell_atlas_coords(Vector2i(x, y)) == Vector2i(0,1) and event.position.x > get_viewport().get_visible_rect().size.x / 2):
+					$Label.text = "You win!"
+					$ShadowRail.visible = false
+			elif(!$Rail.get_point_count()): # If we're placing the first station
+				# Check if the player has clicked on a WEST coast tile
+				if($TileMapLayer.get_cell_atlas_coords(Vector2i(x, y)) == Vector2i(0,1) and event.position.x < get_viewport().get_visible_rect().size.x / 2):
+					$Rail.add_point(Vector2(event.position.x, event.position.y))
+					shadowRail.set(0, event.position)
+					$ShadowRail.visible = true
+		elif event.get_button_index() == 2:
+			$ShadowRail.visible = false
